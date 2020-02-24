a3418906567e89e5f43f3aa34a86da5e34f6d1d6
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-24 20:40:30 +0200

    added a counterSubscription-test (35)

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import { GraphQLServer } from 'graphql-yoga'
+import { GraphQLServer, PubSub } from 'graphql-yoga'
 import db from './db'
+import Subscription from './resolvers/Subscription'
 import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
 import Post from './resolvers/Post'
@@ -8,17 +9,21 @@ import User from './resolvers/User'
 
 // Scalar types - String, Boolean, Int, Float, ID
 
+const pubsub = new PubSub()
+
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers: {
     Query,
     Mutation,
+    Subscription,
     User,
     Post,
     Comment
   },
   context: {
-    db
+    db,
+    pubsub
   }
 })
 
