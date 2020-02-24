e4f6926265a7db1e2711fc0e88a23a29dd8cdbec
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-24 12:18:42 +0200

    added spreadOpertor use, also to .babelrc

--- src/index.js
+++ src/index.js
@@ -169,9 +169,7 @@ const resolvers = {
 
       const user = {
         id: uuidv4(),
-        name: args.name,
-        email: args.email,
-        age: args.age
+        ...args
       }
       users.push(user)
 
@@ -186,10 +184,7 @@ const resolvers = {
 
       const post = {
         id: uuidv4(),
-        title: args.title,
-        body: args.body,
-        published: args.published,
-        author: args.author
+        ...args
       }
       posts.push(post)
 
@@ -205,9 +200,7 @@ const resolvers = {
 
       const comment = {
         id: uuidv4(),
-        text: args.text,
-        author: args.author,
-        post: args.post
+        ...args
       }
       comments.push(comment)
 
