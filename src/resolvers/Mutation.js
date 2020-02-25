e20532f5546372cf2cd1cc2a5d03413e9fe15d8e
Lauri Kyttälä <lauri.kyttala@gmail.com>
2020-02-25 10:06:43 +0200

    created posts subscription

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -67,7 +67,7 @@ const Mutation = {
 
     return user
   },
-  createPost(parent, args, { db }, info) {
+  createPost(parent, args, { db, pubsub }, info) {
     const userExists = db.users.some((user) => user.id === args.data.author)
 
     if (!userExists) {
@@ -79,6 +79,10 @@ const Mutation = {
       ...args.data
     }
     db.posts.push(post)
+
+    if (args.data.published) {
+      pubsub.publish('post', { post })
+    }
 
     return post
   },
